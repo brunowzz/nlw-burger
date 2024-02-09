6008bc301bed67479027dda7f86c8d1d68d7e16a
Bruno Vinícius <94939630+brunowzz@users.noreply.github.com>
2024-02-09 17:55:16 -0300

    feat: create default layout

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View>
+      <Text className="text-white text-2xl font-body">Oi</Text>
+    </View>
+  );
+}
